224100eb8cd9513ce26013f092a76fa45c2e69b2
Lilia Alsadi <22110154@htu.edu.jo>
2026-03-07 17:42:15 +0300

    Added more assets

---
+++ Assets/Lili-UI/Scripts/main/ConfirmationPopup.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmationPopup : MonoBehaviour
+{
+    public Image displayImage;        // confirmation popup preview
+    public Image successDisplayImage; // bought panel preview image
+    public GameObject successPanel;
+
+    private ShopItem pendingItem;
+
+    public void OpenPopup(ShopItem item)
+    {
+        pendingItem = item;
+        displayImage.sprite = item.previewSprite;
+        gameObject.SetActive(true);
+    }
+
+    public void OnYesPressed()
+    {
+        if (pendingItem != null)
+        {
+            pendingItem.CompletePurchase();
+
+            // Show correct sprite in success panel too
+            if (successDisplayImage != null && pendingItem.previewSprite != null)
+                successDisplayImage.sprite = pendingItem.previewSprite;
+
+            GetComponentInParent<UIPanelController>().Close();
+            successPanel.SetActive(true);
+        }
+    }
+
+    public void OnNoPressed()
+    {
+        GetComponentInParent<UIPanelController>().Close();
+    }
+
+    public void OnSuccessPanelClose()
+    {
+        successPanel.SetActive(false);
+    }
+}
